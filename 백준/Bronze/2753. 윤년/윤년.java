4e4cb24e401f79802a9f232c279d17f1f8da4fee
Gujieun <119827260+guz9@users.noreply.github.com>
2023-02-23 20:44:14 +0900

    [Bronze V] Title: 윤년, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,14 +1,16 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);		
-		int year = sc.nextInt();
+	public static void main(String[] args){
 		
-		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-			System.out.println("1");
-		} else {
-			System.out.println("0");			
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		if (year>0 && year <=4000) {
+			if (year%4==0 && year %100 != 0 || year % 400 ==0) {
+				System.out.print(1);
+			}else {
+				System.out.print(0);
+			}
 		}
 	}
 }
