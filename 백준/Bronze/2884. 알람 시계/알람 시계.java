03b852a4c7dc8b711c1b9dd592b5776b753e63b4
Gujieun <119827260+guz9@users.noreply.github.com>
2023-02-23 20:15:00 +0900

    [Bronze III] Title: 알람 시계, Time: 216 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,19 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);		
-
-		int h = sc.nextInt();
-		int m = sc.nextInt();
-		
-		if (m < 45) {
-			h--;
-			h = h < 0 ? 23 : h;
-			System.out.printf("%d %d", h, (m + 60) - 45);
+	public static void main(String[] args){
 			
-		} else {
-			System.out.printf("%d %d", h, m - 45);
+			Scanner sc = new Scanner(System.in);
+			
+			int hour = sc.nextInt();
+			int minute = sc.nextInt();
+			
+			boolean ch = hour > -1 && hour < 24 && minute >-1 && minute < 60;
+			
+			if (ch) {
+				if (minute >= 45) {
+					System.out.printf("%d %d", hour, minute-45);
+				}else if (minute < 45 && hour != 0){
+					System.out.printf("%d %d", hour-1, minute+15);
+				}else {
+					System.out.printf("%d %d", hour+23, minute+15);
+			}
 		}
 	}
 }
