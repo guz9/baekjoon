c2e48f0891829743b36135710c51c405e8f474d9
Gujieun <119827260+guz9@users.noreply.github.com>
2023-02-28 21:35:30 +0900

    [Bronze V] Title: Hello World, Time: 120 ms, Memory: 13852 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String [] arg) {
-        System.out.printf("Hello World!");
+public class Main{
+    public static void main(String[] args){
+        System.out.print("Hello World!");
     }
 }
