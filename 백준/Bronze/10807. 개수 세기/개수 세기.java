47601b20f7f43c6a607f6fb5cd4d4ea4442f8dd4
Gujieun <119827260+guz9@users.noreply.github.com>
2023-03-02 21:08:11 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 17832 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,21 +1,20 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		String n_ = br.readLine();
-		int[] num = new int[n]; 
-		int v = Integer.parseInt(br.readLine());
-		int cnt = 0;
-		
-		for (int i = 0; i < n; ++i) {
-			num[i] = Integer.parseInt(n_.split(" ")[i]);
-			if (v == num[i]) {
-				cnt++;
-			}
-		}
-		System.out.println(cnt);
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int v = scanner.nextInt();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == v) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
